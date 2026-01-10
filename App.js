1284b8735a0cc6dfa8a93f321bdbfdf7eb3c9e9c
MD-Kayesur <rmdkayesur@gmail.com>
2026-01-10 17:54:55 +0600

    today starting app developet

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet,Image,TouchableWithoutFeedback, TouchableOpacity, Text, View,Button } from 'react-native';
+import { useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
+// import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ <View
+  style={{
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+      flexShrink:1,
+  }}
+>
+  <View
+    style={{
+      width: 100,
+      height: 100,
+      backgroundColor: 'green',
+      justifyContent: 'center',
+      alignItems: 'center',
+    
+    }}
+  />
+
+  <View
+    style={{
+      width: 120,
+      height: 100,
+      backgroundColor: 'black',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  />
+  <View
+    style={{
+      width: 120,
+      height: 100,
+      backgroundColor: 'red',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  />
+   
+ 
+
+  <StatusBar style="auto" />
+</View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+   
+ 
